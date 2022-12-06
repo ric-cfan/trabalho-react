1d77215c65fb329a088e2860c5ddb389e5cec72b
RobertoSpa <robertopspagola@gmail.com>
2022-12-06 16:11:23 -0300

    sla

--- src/components/MainLogin/index.jsx
+++ src/components/MainLogin/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
 import "./styles.css";
 import Logo from "../Imagens/box.png";
-import { AuthContext } from "../Contexts/auth"
+import { AuthContext } from "../Contexts/auth";
 import api from "../../services/api";
 
 function MainLogin() {
@@ -9,12 +9,11 @@ function MainLogin() {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
- 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      login(username, password);
 
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(username, password);
+  };
 
   return (
     <form id="card_container" onSubmit={handleSubmit}>
@@ -22,23 +21,25 @@ function MainLogin() {
         <img id="image" src={Logo} alt="" />
 
         <div id="content">
-          <label id="Titulo">Serra<span>Treco</span></label>
+          <label id="Titulo">
+            Serra<span>Treco</span>
+          </label>
           <input
             type="text"
             id="username"
-            placeholder="Digite seu nome de usuário"
+            placeholder="Digite seu email"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-          <input 
-          type="password" 
-          id="password" 
-          placeholder="Digite sua senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-           />
+          <input
+            type="password"
+            id="password"
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button id="submit" >ENTRAR</button>
+        <button id="submit">ENTRAR</button>
       </div>
     </form>
   );
